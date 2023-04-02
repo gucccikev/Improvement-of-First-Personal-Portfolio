ca482aa4c68ff9ffe8da4fabff66c197be8213f3
gucccikev <71460353+gucccikev@users.noreply.github.com>
2023-04-02 18:40:58 +0200

    Started HeaderPage

---
+++ src/main.js
@@ -0,0 +1,8 @@
+function displayHeaderPage() {
+    var headerButton = document.getElementById("headerButton");
+
+    var headerPage = document.getElementById("headerPage");
+    var firstPage = document.getElementById("firstPage");
+    
+    console.log('yooo');
+}
